1c10be3872d95e879db8a97a75bc306945103fa7
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-10 23:19:55 -0500

    feat: render variables values

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,20 @@
 export function MyAwesomeApp() {
+  const name = 'Javiko500';
+  const lastName = 'Gutierrez';
+  const favoriteGames = ['Minecraft', 'League of Legends', 'Fortnite'];
+  const isActive = true;
+  const address = {
+    street: 'Calle de la Paz',
+    number: 123,
+    city: 'Cuenca',
+  }
   return (
     <>
-      <h1>Javiko500</h1>
-      <p>Amy500</p>
+      <h1>{name}</h1>
+      <h3>{lastName}</h3>
+      <p>{favoriteGames.join(', ')}</p>
+      <h1>{isActive ? 'Active' : 'Inactive'}</h1>
+      <pre>{JSON.stringify(address)}</pre>
     </>
   );
 }
