e0446bfb621960acee9f5af58b87e58eb5b235b7
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-10 22:48:51 -0500

    feat: add first component

---
+++ src/FirstStepApp.tsx
@@ -0,0 +1,12 @@
+export function FirstStepsApp() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>I'm a javiko500</p>
+      <button>Click me</button>
+      <div className="">
+        <h2>Into div</h2>
+      </div>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { FirstStepsApp } from './FirstStepApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FirstStepsApp/>
   </StrictMode>,
 )
