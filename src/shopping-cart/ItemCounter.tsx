3ea2962b4bd42924ddad73ee63f224377d7a3655
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-11 22:32:06 -0500

    feat: add or subtract quantity

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
+
 interface ItemCounterProps {
   productName: string;
   quantity?: number;
 }
 export const ItemCounter = ({ productName, quantity = 1 }: ItemCounterProps) => {
+  const [count, setCount] = useState(quantity);
+  const increaseItems = () => {
+    setCount(count + 1);
+  }
+  const decreaseItems = () => {
+    if (count === 1) return;
+    setCount(count - 1);
+  }
   return (
     <section>
       <br />
       <div style={{ marginBottom: '8px' }} >{productName}</div>
-      <button>-1</button>
+      <button onClick={decreaseItems} >-1</button>
       <span style={{
         marginLeft: '10px',
         marginRight: '10px',
-      }}>{quantity}</span>
-      <button>+1</button>
+      }}>{count}</span>
+      <button onClick={increaseItems} >+1</button>
     </section>
   )
 }
