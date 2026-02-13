1edf919f5d14f669bcadd971e1be62dab3910a5f
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-12 23:34:01 -0500

    text: component MyAwesomeApp

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -1,11 +1,25 @@
-import { describe, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { MyAwesomeApp } from './MyAwesomeApp';
 import { render, screen } from '@testing-library/react';
 describe('MyAwesomeApp.test', () => {
   test( 'should render first name and last name', () => {
+    const { container } = render(<MyAwesomeApp />);
+    screen.debug();
+
+    const h1 = container.querySelector('h1');
+    const h3  = container.querySelector('h3');
+
+    expect(h1?.innerHTML).toContain('Javiko500');
+    expect(h3?.innerHTML).toContain('Gutierrez');
+
+  });
+
+  test( 'should render first name and last name', () => {
     render(<MyAwesomeApp />);
     screen.debug();
-    console.log('<--------------- JK MyAwesomeApp.test --------------->');
-    // console.log(container.innerHTML);
+
+    const h1 = screen.getByRole('heading', {});
+
+    expect(h1?.innerHTML).toContain('Javiko500');
   });
 });
