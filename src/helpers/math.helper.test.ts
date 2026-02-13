096f13a6945ba54d1ed5853e8f3b74282c5b5135
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-12 22:16:24 -0500

    feat: add test in add, subtract and multiply

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
-import { add } from './math.helper';
-describe('Math.helper.test', () => {
+import { add, multiply, subtract } from './math.helper';
+describe('Math.helper.test add', () => {
   test( 'should add two positive numbers', () => {
     // ! 1 Arrange
     const a = 1;
@@ -9,5 +9,48 @@ describe('Math.helper.test', () => {
     const result = add( a,b );
     // ! 3 Assert
     expect( result ).toBe( 2 );
+  });
+});
+
+describe('Math.helper.test subtract', () => {
+  test( 'should subtract two positive numbers', () => {
+    // ! 1 Arrange
+    const a = 1;
+    const b = 1;
+    // ! 2 act
+    const result = subtract( a,b );
+    // ! 3 Assert
+    expect( result ).toBe( 0 );
+  });
+
+  test( 'should return a negative number ', () => {
+    // ! 1 Arrange
+    const a = 1;
+    const b =  5;
+    // ! 2 act
+    const result = subtract( a,b );
+    // ! 3 Assert
+    expect( result ).toBe( -4 );
+  });
+});
+
+describe('Math.helper.test multiply', () => {
+  test( 'Should return 4', () => {
+    // ! Arrange
+    const a = 2;
+    const b = 2;
+    // ! Act
+    const result = multiply( a,b );
+    // ! Assert
+    expect( result ).toBe( 4 );
+  });
+  test( 'Should return -4', () => {
+    // ! Arrange
+    const a = 2;
+    const b = -2;
+    // ! Act
+    const result = multiply( a,b );
+    // ! Assert
+    expect( result ).toBe( -4 )
   });
 });
