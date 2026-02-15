da8f96eff6486b63177c721f3e53f06355d25345
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-14 20:11:17 -0500

    test: mock component ItemCounter

--- src/FirstStepApp.test.tsx
+++ src/FirstStepApp.test.tsx
@@ -1,9 +1,19 @@
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 import { FirstStepsApp } from './FirstStepApp';
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+  ItemCounter: () => <div>Mocked ItemCounter</div>,
+}));
+
 describe('FirstStepApp.test', () => {
   test( 'should match with snapshot', () => {
     const { container } = render(<FirstStepsApp />);
     expect(container).toMatchSnapshot();
+  });
+
+  test( 'should render the correct number of ItemCounter component', () => {
+    render(<FirstStepsApp />);
+    expect( screen.getAllByText('Mocked ItemCounter') ).toHaveLength(3);
   });
 });
