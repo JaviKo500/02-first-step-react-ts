77a37e3a777a3a8a80e28497ca9940f17bbdad50
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-14 20:31:31 -0500

    test: mock fn functions and test params called

--- src/FirstStepApp.test.tsx
+++ src/FirstStepApp.test.tsx
@@ -1,12 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import { FirstStepsApp } from './FirstStepApp';
 
+const mockItemCounter = vi.fn( () =>  {
+  return <div>Mocked ItemCounter</div>;
+});
+
 vi.mock('./shopping-cart/ItemCounter', () => ({
-  ItemCounter: () => <div>Mocked ItemCounter</div>,
+  ItemCounter:( props: unknown ) => mockItemCounter(props as any),
 }));
 
+// vi.mock('./shopping-cart/ItemCounter', () => ({
+//   ItemCounter: ( props: unknown ) => 
+//     <div data-testid="mocked-item-counter" name= {props.productName} quantity={props.quantity}>
+//       Mocked ItemCounter
+//     </div>,
+// }));
+
 describe('FirstStepApp.test', () => {
+
+  afterEach( () => {
+    vi.clearAllMocks();
+  } )
+
   test( 'should match with snapshot', () => {
     const { container } = render(<FirstStepsApp />);
     expect(container).toMatchSnapshot();
@@ -15,5 +31,13 @@ describe('FirstStepApp.test', () => {
   test( 'should render the correct number of ItemCounter component', () => {
     render(<FirstStepsApp />);
     expect( screen.getAllByText('Mocked ItemCounter') ).toHaveLength(3);
+  });
+
+  test( 'should render ItemCounter with correct params', () => {
+    render(<FirstStepsApp />);
+    expect(mockItemCounter).toHaveBeenCalledTimes(3);
+    expect(mockItemCounter).toHaveBeenCalledWith({ productName: 'Plays station 5', quantity: 1 });
+    expect(mockItemCounter).toHaveBeenCalledWith({ productName: 'Xbox one', quantity: 1 });
+    expect(mockItemCounter).toHaveBeenCalledWith({ productName: 'Azus Rog', quantity: 1 });
   });
 });
